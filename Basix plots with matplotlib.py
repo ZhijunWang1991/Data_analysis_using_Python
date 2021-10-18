cdd0e0f080a132855edb7fb16913cd07da3793d9
ZhijunWang1991 <zhijun0916@gmail.com>
2021-10-18 15:25:18 +0200

    pandas

--- Basix plots with matplotlib.py
+++ Basix plots with matplotlib.py
@@ -65,3 +65,28 @@ europe['italy'] = data
 
 # Print europe
 print(europe)
+
+
+# Import csv
+# Import pandas as pd
+import pandas as pd
+
+# Import the cars.csv data: cars
+cars = pd.read_csv('cars.csv')
+
+# Print out cars
+print(cars)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
